c289f6eb23aeb15e444b021617fb9889a9def3f0
FaStNiNjAzZ <47434948+FaStNiNjAzZ@users.noreply.github.com>
2020-03-18 16:21:25 +0000

    no message

---
+++ Isometric Game Actually Gonna Work/Should be Working/Assets/Scripts/LoadGameScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gameSave1()
+    {
+        if (File.Exists("SaveGame1.txt"))
+        {
+            using (StreamWriter sw = File.CreateText("errorlog.txt"))
+            {
+                sw.WriteLine("Message --- {0}", ex.Message);
+            }
+        }
+    }
+}
